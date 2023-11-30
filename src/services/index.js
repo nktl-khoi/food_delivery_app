d83b79dea85743ca05560d7947ca4a2f7c4facd5
Phung Tran Dang Khoi <20520150@gm.uit.edu.vn>
2023-11-30 06:20:18 -0800

    HomeScreen

---
+++ src/services/index.js
@@ -0,0 +1,11 @@
+import StaticImageService from './StaticImageService';
+import AuthenicationService from './AuthenticationService';
+import StorageService from './StorageService';
+import RestaurantService from './RestaurantService';
+
+export {
+  StaticImageService,
+  AuthenicationService,
+  StorageService,
+  RestaurantService,
+};
