8de133b705868db8b6fcc2af5c637d9d387dd47c
Phung Tran Dang Khoi <20520150@gm.uit.edu.vn>
2023-12-27 17:20:04 -0800

    food dt and vendor dt

---
+++ src/screens/vendor_details/AllFoodInShopGridDisplay.tsx
@@ -0,0 +1,39 @@
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import {SectionText} from '../../components/texts/SectionText';
+import VerticalListFood from '../../components/displays/VerticalListFood';
+import {useQuery} from '@apollo/client';
+import {GET_ALL_PRODUCT_OF_SHOP} from './VendorDetailsQuery';
+
+type ThisProps = {
+  navigation: any;
+  route: any;
+};
+
+export default function AllFoodInShopGridDisplay(
+  props: ThisProps,
+): JSX.Element {
+  const {data, loading} = useQuery(GET_ALL_PRODUCT_OF_SHOP, {
+    variables: {
+      getAllProductOfShopId: props.route.params.shopId,
+    },
+  });
+
+  if (loading) return <ActivityIndicator size={'small'} />;
+  return (
+    <View style={styles.container}>
+      <VerticalListFood
+        data={data?.getAllProductOfShop}
+        navigation={props.navigation}
+        listHeaderComponent={<SectionText>All favorite foods</SectionText>}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 12,
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+  },
+});
